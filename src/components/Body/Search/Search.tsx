cb09f1c113acd2bbd8715666111f69e5b95cd2a8
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-10-02 04:52:15 +0530

    Update Search.tsx

--- src/components/Body/Search/Search.tsx
+++ src/components/Body/Search/Search.tsx
@@ -36,13 +36,13 @@ function Search() {
 
   return (
     <form onSubmit={handleSearchSubmit} className='min-h-screen bg-blue-200 w-full'>
-      <div className='h-16 pt-6 justify-between w-full px-24 sm:flex'>
+      <div className='h-16 pt-6 justify-between w-full px-6 sm:px-24 sm:flex'>
         {/* Input field for search */}
         <Input
           placeholder='Search for a city'
           value={searchCity}
           onChange={handleCityChange}
-          className='border inline-block border-blue-400 w-[90%] rounded-md h-12'
+          className='border inline-block border-blue-400 w-[90%] sm:w-[70%] rounded-md h-12 mb-4 sm:mb-0'
           aria-label='Search for a city'
         />
         <div className='flex p-0 justify-center'>
@@ -68,9 +68,11 @@ function Search() {
           </Button>
         </div>
       </div>
-      <div className='min-h-screen bg-blue-200 flex w-full'>
+
+      {/* Weather Card with added margin and centered alignment */}
+      <div className='min-h-screen bg-blue-200 flex items-center justify-center py-8'>
         {/* Conditionally render Weather Card */}
-        {<WeatherCard className='self-center' />}
+        {<WeatherCard className='self-center mx-4 p-4 bg-white shadow-lg rounded-lg' />}
       </div>
     </form>
   );
