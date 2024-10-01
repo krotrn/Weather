7177f42f1b89e6dce2fb008e14a8e44ef6f0d55c
krotrn <karankumarsahtkg2018@gmail.com>
2024-10-01 16:17:17 +0530

    Refactor Icon component to remove unused import and optimize code

--- src/components/Body/Home/Card/Icon.tsx
+++ src/components/Body/Home/Card/Icon.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Icon({src,alt,className}: {src?: string, alt?: string, className?: string}) {
   return (
     <img src={src} alt={alt} className={className} />
--- src/components/Body/Home/Home.tsx
+++ src/components/Body/Home/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import WeatherCard from './Card/WeatherCard'
 
 function Home() {
