2cd0c4b0898ae26d0b60c93ea0d1583372066847
krotrn <karankumarsahtkg2018@gmail.com>
2024-10-03 02:11:46 +0530

    Refactor location error display in Search component

--- src/components/Body/Search/Search.tsx
+++ src/components/Body/Search/Search.tsx
@@ -64,7 +64,7 @@ function Search() {
       {/* Display any errors from location fetching */}
 
 {locationError && (
-  <div className="flex items-center p-4 my-24 md:my-4 text-sm text-red-700 bg-red-100/30 backdrop-blur-md rounded-lg shadow-lg m-5 border border-white/20" role="alert">
+  <div className="flex items-center p-4 my-24 text-sm text-red-700 bg-red-100/30 backdrop-blur-md rounded-lg shadow-lg m-5 border border-white/20" role="alert">
     <svg
       aria-hidden="true"
       className="w-5 h-5 mr-2 text-red-700"
@@ -81,9 +81,6 @@ function Search() {
     <span className="font-medium">Location Error:</span> {locationError}
   </div>
 )}
-
-
-
       {/* Weather card */}
       <div className='flex flex-col items-center justify-center w-full'>
         <div className="w-full flex justify-center">
